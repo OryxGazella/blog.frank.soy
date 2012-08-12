1521012aecff235454db83b72ad4fc5246bd4370
Frank Smith <oryx.gazella@gmail.com>
2012-08-12 10:36:36 +0200

    Added some styling to the main page

--- Gemfile
+++ Gemfile
@@ -6,7 +6,8 @@ gem 'rails', '3.2.7'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 group :development, :test do
-	gem 'sqlite3'
+  gem 'sqlite3'
+  gem 'faker'
 end
 
 # Gems used only for assets and not required
@@ -29,6 +30,7 @@ end
 gem 'jquery-rails'
 gem 'thin'
 gem 'redcarpet'
+gem 'pygmentize'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,16 @@ class Post < ActiveRecord::Base
   private
   def render_body
     require 'redcarpet'
-    renderer = Redcarpet::Render::HTML.new
+    renderer = PygmentizeHTML 
     extensions = {fenced_code_blocks: true}
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     self.rendered_body = redcarpet.render self.body
+  end
+end
+
+class PygmentizeHTML < Redcarpet::Render::HTML
+  def block_code(code, language)
+    require 'pygmentize'
+    Pygmentize.process(code, language)
   end
 end
