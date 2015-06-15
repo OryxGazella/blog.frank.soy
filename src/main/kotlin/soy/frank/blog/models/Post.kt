06a9b13a2b9f233203ee40d2c8e6ab22e2a4eff2
Frank Smith <labiniboes+git@gmail.com>
2015-06-15 23:58:34 +0200

    Connect to local Postgres database.

--- src/main/kotlin/soy/frank/blog/models/Post.kt
+++ src/main/kotlin/soy/frank/blog/models/Post.kt
@@ -2,10 +2,13 @@ package soy.frank.blog.models
 
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.Table
 
 
 Entity
+Table(name = "posts")
 data class Post(
         Id
         val id: Long = -1,
-        val title: String = "")
+        val title: String = "",
+        val renderedBody: String = "")
