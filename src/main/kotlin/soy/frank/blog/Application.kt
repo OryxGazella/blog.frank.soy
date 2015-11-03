e7c7eaff5e4c33e021952290542cbe8583a2b413
Frank Smith <oryx.gazella@gmail.com>
2015-11-03 22:54:11 +0200

    Remove redundant public declarations in Application class

--- src/main/kotlin/soy/frank/blog/Application.kt
+++ src/main/kotlin/soy/frank/blog/Application.kt
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-public open class Application {
+open class Application {
     companion object {
-        @JvmStatic public fun main(args: Array<String>) {
+        @JvmStatic fun main(args: Array<String>) {
             SpringApplication.run(Application::class.java, *args)
         }
     }
