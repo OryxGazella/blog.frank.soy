754db88d0dd02b527322922e7080e1875b8b6eea
Frank Smith <oryx.gazella@gmail.com>
2016-07-31 02:50:50 +0200

    Make funcitonal tests go to the real website

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -2,12 +2,12 @@ import static io.netty.handler.codec.http.HttpResponseStatus.MOVED_PERMANENTLY
 import static ratpack.groovy.Groovy.ratpack
 import asset.pipeline.ratpack.AssetPipelineModule
 
-def old_posts = [9: "DTOs with care",
-                 8: "A touch of style",
-                 7: "What do to when you have strayed from the path of the Test Driven Ninja",
-                 6: "RIP: The Monolithic Blogpost 2012-2012",
-                 5: "Coded in Braam: September 2012",
-                 3: "Security: art brut"]
+def old_posts = [9: "dtos-with-care.html",
+                 8: "a-touch-of-style.html",
+                 7: "tdd-ninja.html",
+                 6: "rip-the-monolithic-blogpost.html",
+                 5: "coded-in-braam.html",
+                 3: "security-art-brut.html"]
 
 ratpack {
     bindings {
@@ -20,9 +20,9 @@ ratpack {
             redirect "/posts/i-code-java-2016.html"
         }
         old_posts.each {
-            def slug = it.key
+            def slug = it.value
             get("posts/${it.key}") {
-                redirect MOVED_PERMANENTLY.code(), "http://frank.soy/posts/$slug"
+                redirect MOVED_PERMANENTLY.code(), "/posts/$slug"
             }
         }
     }
