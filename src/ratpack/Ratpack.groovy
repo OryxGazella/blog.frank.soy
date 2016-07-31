16a65a950635c9ff7059da3462baa05d8ce6b2e9
Frank Smith <oryx.gazella@gmail.com>
2016-07-31 02:28:30 +0200

    Add functional testing
    Currently uses the existing blog for verification

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,9 +1,24 @@
+import static io.netty.handler.codec.http.HttpResponseStatus.MOVED_PERMANENTLY
+
 import static ratpack.groovy.Groovy.ratpack
+
+def old_posts = [9: "DTOs with care",
+                8: "A touch of style",
+                7: "What do to when you have strayed from the path of the Test Driven Ninja",
+                6: "RIP: The Monolithic Blogpost 2012-2012",
+                5: "Coded in Braam: September 2012",
+                3: "Security: art brut"]
 
 ratpack {
     handlers {
-        get {
-            render "Frank's Blog"
+        get("icj2016") {
+            redirect "/posts/i-code-java-2016.html"
+        }
+        old_posts.each {
+            def slug = it.key
+            get("posts/${it.key}") {
+                redirect MOVED_PERMANENTLY.code(), "http://frank.soy/posts/$slug"
+            }
         }
     }
 }
---
+++ src/test/groovy/BlogSpec.groovy
@@ -0,0 +1,33 @@
+import geb.spock.GebSpec
+import ratpack.groovy.test.GroovyRatpackMainApplicationUnderTest
+import spock.lang.Shared
+import spock.lang.Unroll
+
+class BlogSpec extends GebSpec {
+
+    @Shared
+    def aut = new GroovyRatpackMainApplicationUnderTest()
+
+    def setup() {
+        URI base = aut.address
+        browser.baseUrl = base.toString()
+    }
+
+    @Unroll("GET to /posts/#Old_Post_Id redirects to the static page with title: #Post_Title")
+    def "Renders a redirect for existing posts"() {
+        when:
+        go "posts/${Old_Post_Id}"
+
+        then:
+        title == Post_Title
+
+        where:
+        Old_Post_Id | Post_Title
+        "9"         | "DTOs with care"
+        "8"         | "A touch of style"
+        "7"         | "What do to when you have strayed from the path of the Test Driven Ninja"
+        "6"         | "RIP: The Monolithic Blogpost 2012-2012"
+        "5"         | "Coded in Braam: September 2012"
+        "3"         | "Security: art brut"
+    }
+}
